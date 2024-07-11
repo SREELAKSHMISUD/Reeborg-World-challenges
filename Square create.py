169dbca7cf784267a8339aa5fe9e80bbc3a2644b
SREELAKSHMI SUDHEER 1111 <121602892+SREELAKSHMISUD@users.noreply.github.com>
2024-07-11 21:27:12 +0530

    Add files via upload

---
+++ Square create.py
@@ -0,0 +1,18 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
+    
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
